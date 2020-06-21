c450da1d47b21eb1bee53daf85e359f34bfd17a6
systemthinker <robbertoutenaar@gmail.com>
2020-06-21 21:20:55 +0200

    product Details router

--- routers/basket.js
+++ routers/basket.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Baskets = require('../models').basket 
 const Products = require('../models').product 
 const Client = require('../models').client
+const BasketProducts = require('../models').basketProduct
 
 
 const router = new Router();
@@ -17,20 +18,92 @@ router.get('/:id', async(req,res,next)=>{
 })
 
 router.patch('/:id', async(req,res,next)=>{
+    // adds item to basket
+    try{
     const id = req.params.id
+    if(!id){
+        res.status(400).send('no basket found')
+    }
+
+    const basketFound = await Baskets.findByPk(id)
+    console.log("basketFound", basketFound)
+    if(!basketFound) {
+        res.status(400).send('no basket found')
+    }
+    const productId = req.body.productId
+
     
+    
+    if(!productId){
+        res.status(400).send('no product found to add')
+    }
+
+    const validProductId = await Products.findByPk(productId)
+
+    if(!validProductId) {
+        res.status(400).send('no valid product to add, please provide an existing product')
+    }
+
+    const newProductAddedToBasket = await BasketProducts.create({
+        basketId : id,
+        productId
+    })
+
     const basket = await Baskets.findByPk(id,{
         include: [Products]
     })
-
-     // later add logic for editing basket i.e. remove a product.
-   
-    
+ 
     res.status(200).send(basket);
+    } catch(e){
+        next(e)
+    }
 
 })
 
-router.post('/:id', async(req,res,next)=>{
+router.delete('/:id', async(req,res,next)=>{
+    // removes product from basket
+
+    try{
+        const id = req.params.id
+        if(!id){
+            res.status(400).send('no basket found')
+        }
+    
+        const basketFound = await Baskets.findByPk(id)
+        
+        if(!basketFound) {
+            res.status(400).send('no basket found')
+        }
+        const productId = req.body.productId
+           
+        if(!productId){
+            res.status(400).send('no product found to add')
+        }
+    
+        const validProductId = await Products.findByPk(productId)
+    
+        if(!validProductId) {
+            res.status(400).send('no valid product found to remove, please provide an existing product')
+        }
+
+        await BasketProducts.destroy(
+            {where: {productId},
+        limit: 1},
+            )
+
+        const newBasket = await Baskets.findByPk(id,{
+            include: [Products]
+        })
+        
+        res.status(200).send(newBasket)
+
+    } catch(e){
+        next(e)
+    }
+
+})
+
+router.get('/create/:id', async(req,res,next)=>{
     id = req.params.id; 
        
     try{
@@ -47,34 +120,58 @@ router.post('/:id', async(req,res,next)=>{
         return
     }
 
-    if(!req.body.id){
-        res.status(400).send('no product found')
-        return
-    }
-    
-    const clientHasBasket = await Baskets.findOne({
+    let basket = await Baskets.findOne({
         where: {
             clientId : req.params.id
         }
     })
 
-    if(!clientHasBasket){
-    const basket = await Baskets.create({
+    
+
+    if(!basket){
+    const basketCreated = await Baskets.create({
         clientId: id
     })
-    console.log('new basket is', basket)
+    
+    res.status(200).send(basketCreated)
     }
 
-    // add logic for adding products.
-
-    console.log('clients basket is', clientHasBasket)
-
-
-    res.status(200).send('ok')
+    res.status(200).send(basket)
     } catch(e){
     next(e)
     }
 
 })  
+
+router.delete('/all/:id', async(req,res,next)=>{
+
+    try{
+        const id = req.params.id
+        if(!id){
+            res.status(400).send('no basket found')
+        }
+    
+        const basketFound = await Baskets.findByPk(id)
+        
+        if(!basketFound) {
+            res.status(400).send('no basket found')
+        }
+
+        await BasketProducts.destroy(
+            {where: {
+                basketId: id}
+            })
+
+        const newBasket = await Baskets.findByPk(id,{
+            include: [Products]
+        })
+        
+        res.status(200).send(newBasket)
+
+    } catch(e){
+        next(e)
+    }
+
+})
 
 module.exports = router;
--- routers/productDetails.js
+++ routers/productDetails.js
@@ -1,9 +1,29 @@
 const { Router } = require("express");
-const Client = require("../models/").client;
+const Products = require("../models/").product;
 
 const router = new Router();
 
+router.get('/:id', async(req,res,next)=>{
+    try{
+        const id = req.params.id
+        if(!id){
+            res.status(400).send('no product found')
+        }
 
+        const product = await Products.findByPk(id)
+
+        if(!product){
+            res.status(400).send('no product found')
+        }
+
+        res.status(200).send(product)
+
+
+
+    } catch(e){
+        next(e)
+    }
+})
 
 
 module.exports = router;
