0996710f1a5b4ad4b61c9b0c7f1b857cf52dbd38
systemthinker <robbertoutenaar@gmail.com>
2021-09-26 13:48:56 +0200

    cors update

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require("express");
-const loggerMiddleWare = require("morgan");
-const corsMiddleWare = require("cors");
-const { PORT } = require("./config/constants");
-const authRouter = require("./routers/auth");
-const clientRouter = require("./routers/client");
-const basketRouter = require("./routers/basket");
-const orderRouter = require("./routers/order");
-const productRouter = require("./routers/product");
-const productDetailRouter = require("./routers/productDetails");
-const authMiddleWare = require("./auth/middleware");
+const express = require('express');
+const loggerMiddleWare = require('morgan');
+const corsMiddleWare = require('cors');
+const { PORT } = require('./config/constants');
+const authRouter = require('./routers/auth');
+const clientRouter = require('./routers/client');
+const basketRouter = require('./routers/basket');
+const orderRouter = require('./routers/order');
+const productRouter = require('./routers/product');
+const productDetailRouter = require('./routers/productDetails');
+const authMiddleWare = require('./auth/middleware');
 
 const app = express();
-app.use(loggerMiddleWare("dev"));
+app.use(loggerMiddleWare('dev'));
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
-app.use(corsMiddleWare({ origin: true }));
+app.use(
+  corsMiddleWare({
+    origin: 'https://www.suusjepluusje.nl',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
 
 if (process.env.DELAY) {
   app.use((req, res, next) => {
@@ -22,9 +28,9 @@ if (process.env.DELAY) {
   });
 }
 
-app.post("/authorized_post_request", authMiddleWare, (req, res) => {
+app.post('/authorized_post_request', authMiddleWare, (req, res) => {
   const user = req.user;
-  delete user.dataValues["password"];
+  delete user.dataValues['password'];
   res.json({
     youPosted: {
       ...req.body,
@@ -35,12 +41,12 @@ app.post("/authorized_post_request", authMiddleWare, (req, res) => {
   });
 });
 
-app.use("/", authRouter);
-app.use("/client", clientRouter);
-app.use("/basket", basketRouter);
-app.use("/order", orderRouter);
-app.use("/product", productRouter);
-app.use("/details", productDetailRouter);
+app.use('/', authRouter);
+app.use('/client', clientRouter);
+app.use('/basket', basketRouter);
+app.use('/order', orderRouter);
+app.use('/product', productRouter);
+app.use('/details', productDetailRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
